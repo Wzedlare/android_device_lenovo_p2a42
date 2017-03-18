1b7f7d6dadce709fb53d30ecf548f95337ac90c8
wzedlare <vedatak01@gmail.com>
2017-03-18 11:14:48 +0000

    p2a42: Update path to kernel source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,8 +48,8 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_CONFIG := lineageos_mido_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
+TARGET_KERNEL_CONFIG := lineageos_p2a42_defconfig
+TARGET_KERNEL_SOURCE := kernel/lenovo/msm8953
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/Image.gz-dtb
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
